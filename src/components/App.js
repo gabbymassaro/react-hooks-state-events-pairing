5a58d0cdd678af73f86caed1f64993d263211494
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-16 19:44:48 -0400

    Create title, view, upload, upvote, and downvote

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,36 @@
 import video from "../data/video.js";
 
+import React, { useState } from "react";
+
 function App() {
-  console.log("Here's your data:", video);
+
+  const [upVotes, setUpVotes] = useState(video.upvotes)
+  const [downVotes, setDownVotes] = useState(video.downvotes)
+
+  function onUpVote() {
+    const newUpVote = upVotes + 1
+    setUpVotes(newUpVote)
+  }
+
+  function onDownVote() {
+    const newDownVote = downVotes + 1
+    setDownVotes(newDownVote)
+  }
 
   return (
     <div className="App">
       <iframe
         width="919"
         height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        src="https://www.youtube.com/embed/dpw9EHDh2bM"
         frameBorder="0"
         allowFullScreen
         title="Thinking in React"
       />
+      <h1>{video.title}</h1>
+      <p>{video.views} Views | Uploaded {video.createdAt}</p>
+      <button onClick={onUpVote}>{upVotes} 👍</button>
+      <button onClick={onDownVote}>{downVotes} 👎</button>
     </div>
   );
 }
