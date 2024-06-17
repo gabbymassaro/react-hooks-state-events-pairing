044c7cff3eccffa5de26133b3478583eb13c3840
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-16 21:01:02 -0400

    Add a bonus feature: add new comments

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,15 +1,36 @@
 import React, { useState } from "react";
 import Comment from "./Comment.js";
+import { v4 as uuid } from "uuid";
 
 function CommentList({ comments }) {
   const [showComments, setShowComments] = useState(true);
+  const [commentList, setCommentList] = useState(comments)
+  const [newComment, setNewComment] = useState("")
 
   function onHideClick() {
     setShowComments((show) => !show);
   }
 
+  function handleValue(event) {
+    setNewComment(event.target.value)
+  }
+
+  function addNewComment(event) {
+    event.preventDefault()
+    const newCommentList = ({
+      id: uuid(),
+      user: `User ${uuid()}`,
+      comment: newComment
+    })
+    setCommentList([...commentList, newCommentList])
+  }
+
   return (
     <div>
+      <br />
+      <form onSubmit={addNewComment}>Add New Comment:
+        <input onChange={handleValue}></input>
+      </form>
       <br />
       <button onClick={onHideClick}>
         {showComments ? "Hide Comments" : "Show Comments"}
@@ -17,9 +38,9 @@ function CommentList({ comments }) {
       <hr />
       {showComments ? (
         <>
-          <h2>{comments.length} Comments</h2>
+          <h2>{commentList.length} Comments</h2>
           <div>
-            {comments.map((comment) => (
+            {commentList.map((comment) => (
               <Comment
                 key={comment.id}
                 user={comment.user}
